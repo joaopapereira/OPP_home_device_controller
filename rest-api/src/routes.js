6ae7d1cde6563731ebfba54576f83e8e473d8649
Joao Pereira <joaopapereira@gmail.com>
2017-10-09 18:48:25 -0400

    change of structure

--- rest-api/src/routes.js
+++ rest-api/src/routes.js
@@ -1,4 +1,8 @@
+let server = require('iotivity-adapter/domain/server');
+
 module.exports = function (app) {
+  console.log("server:", server());
+  
   app.get('/', function (req, res) {
     res.send('Open Peer Power API');
   });
